b235d2b99c585f03f9f51e829a38da5916e62e0c
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-05-10 11:27:32 +0600

    attribute manipulation

---
+++ Learn-jQuery-From-Anisul_Islam/tutorial 3  attribute manipulation/attribute.js
@@ -0,0 +1,11 @@
+//Access Attribute by using JavaScript
+
+// console.log(document.querySelector("a").getAttribute("href")); 
+
+// $("a").attr("href");
+
+//  $("a").removeattr("href");
+
+// document.querySelector("a").setAttribute("href","https://bonikbarta.net");//change attribute by js 
+
+$("a").attr("href","https://bonikbarta.net");
