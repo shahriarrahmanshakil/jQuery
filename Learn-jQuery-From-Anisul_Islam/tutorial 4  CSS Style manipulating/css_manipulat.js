7d29a62277b3b0414241f35e4c019b4b08ecdc8c
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-05-11 10:46:03 +0600

    css style manipulating added

---
+++ Learn-jQuery-From-Anisul_Islam/tutorial 4  CSS Style manipulating/css_manipulat.js
@@ -0,0 +1,15 @@
+// $("h1").css("color","tomato");
+// $("h1").css("font-size","3rem");
+
+
+//using json
+// $("h1").css({
+//     "color":"green", "font-style" : "italic"
+// });
+
+// using js 
+// document.querySelector("h1").classList.remove("heading");
+
+
+//using jquery
+$("h1").addClass("heading");
