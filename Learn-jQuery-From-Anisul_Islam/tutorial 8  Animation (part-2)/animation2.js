7dd1888ac938a54ca4b04a6870a53f3a0701eea8
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-05-12 18:20:26 +0600

    animation part-2 added

---
+++ Learn-jQuery-From-Anisul_Islam/tutorial 8  Animation (part-2)/animation2.js
@@ -0,0 +1,8 @@
+
+$("#mybtn").click(function(){
+    // $("#mydiv").animate({opacity:"0.5"}),500000;
+    $("#mydiv").animate({   height:"700px",
+                            weight:"9000px",
+
+                        }),2000;
+})
